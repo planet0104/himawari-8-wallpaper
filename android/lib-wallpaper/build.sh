3a61ada091fb15d975546431851ef966f0806c17
JiaYe <planet0104@gmail.com>
2019-03-01 10:22:30 +0800

    openssl build pass.

--- android/lib-wallpaper/build.sh
+++ android/lib-wallpaper/build.sh
@@ -1,15 +1,17 @@
-#cargo:rerun-if-env-changed=ARMV7_LINUX_ANDROIDEABI_OPENSSL_LIB_DIR
-#cargo:rerun-if-env-changed=OPENSSL_LIB_DIR
-#cargo:rerun-if-env-changed=ARMV7_LINUX_ANDROIDEABI_OPENSSL_INCLUDE_DIR
-#cargo:rerun-if-env-changed=OPENSSL_INCLUDE_DIR
-#cargo:rerun-if-env-changed=ARMV7_LINUX_ANDROIDEABI_OPENSSL_DIR
-#cargo:rerun-if-env-changed=OPENSSL_DIR
+#~/.bashrc 中配置
+# export ANDROID_NDK="$HOME/android-ndk-r19b"
+# export ANDROID_TOOLCHAIN="$HOME/ndk-standalone-16-arm"
+# export PATH="$PATH:$ANDROID_TOOLCHAIN/bin"
 
-# export OPENSSL_DIR="/usr/local/openssl"
-export OPENSSL_DIR="/openssl_android/lib/armeabi-v7a"
-export RUST_BACKTRACE=1
-export OPENSSL_STATIC="TRUE"
-export OPENSSL_INCLUDE_DIR="~/openssl_android/lib/armeabi-v7a/include"
+#.cargo/config中配置
+# [target.arm-linux-androideabi]
+# ar = "~/ndk-standalone-16-arm/bin/arm-linux-androideabi-ar"
+# linker = "~/ndk-standalone-16-arm/bin/arm-linux-androideabi-gcc"
 
-cargo build --target armv7-linux-androideabi
+#参考 https://github.com/sunsheng/rust-android-https/blob/master/build.sh
+#编译好的openssl下载 https://github.com/leenjewel/openssl_for_ios_and_android
+export OPENSSL_INCLUDE_DIR=/home/planet/armeabi-v7a/include
+export OPENSSL_LIB_DIR=/home/planet/armeabi-v7a/lib
+
+OPENSSL_STATIC=yes cargo build --target armv7-linux-androideabi
 cp target/armv7-linux-androideabi/debug/libwallpaper.so ../h8w-android/app/src/main/jniLibs/armeabi-v7a/libwallpaper.so
