e3cae27939b86b787721ab8a85c22ba7c3e532b7
planet0104 <planet0104@gmail.com>
2019-03-01 10:38:20 +0800

    lib openssl files

--- android/lib-wallpaper/build.cmd
+++ android/lib-wallpaper/build.cmd
@@ -1,3 +1,5 @@
-set OPENSSL_DIR=C:\OpenSSL-Win64
+set OPENSSL_INCLUDE_DIR=C:\Users\days888\Documents\GitHub\himawari-8-wallpaper\android\lib-wallpaper\lib_openssl_armeabi-v7a\include
+set OPENSSL_LIB_DIR=C:\Users\days888\Documents\GitHub\himawari-8-wallpaper\android\lib-wallpaper\lib_openssl_armeabi-v7a\lib
+set OPENSSL_STATIC=yes
 cargo build --target armv7-linux-androideabi
 copy target\armv7-linux-androideabi\debug\libwallpaper.so ..\h8w-android\app\src\main\jniLibs\armeabi-v7a\libwallpaper.so
