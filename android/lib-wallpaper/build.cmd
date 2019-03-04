160ad87089a0271f027b5f9036f32dd2efa015b8
planet0104 <planet0104@gmail.com>
2019-03-04 10:14:03 +0800

    windows arm编译不过

--- android/lib-wallpaper/build.cmd
+++ android/lib-wallpaper/build.cmd
@@ -1,3 +1,6 @@
-set PATH=%PATH%;F:\ndk-standalone-16-arm\bin
-cargo build --target armv7-linux-androideabi
-copy target\armv7-linux-androideabi\debug\libwallpaper.so ..\h8w-android\app\src\main\jniLibs\armeabi-v7a\libwallpaper.so
+set ANDROID_NDK="F:\android-ndk-r18b"
+set ANDROID_TOOLCHAIN=F:\ndk-standalone-16-arm\bin
+::设置要临时加入到path环境变量中的路径
+set PATH=%PATH%;%ANDROID_TOOLCHAIN%
+cargo build --target arm-linux-androideabi
+copy target\arm-linux-androideabi\debug\libwallpaper.so ..\h8w-android\app\src\main\jniLibs\armeabi\libwallpaper.so
